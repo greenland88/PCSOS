a98bbb6d1636630df4b11e5be935dc193a058553
PCS OS Maintainers <pcs-os@localhost>
2026-09-02 12:54:44 -0700

    chore: export pool shortlist adapter

--- src/pcs/pool/__init__.py
+++ src/pcs/pool/__init__.py
@@ -11,10 +11,12 @@ from .models import (
 )
 from .runner import run_pcs_pool
 from .final_gates import PoolEventResult, evaluate_pool_event
+from .options import SpreadCandidate, shortlist_spreads
 
 __all__ = [
     "EligibilityStatus", "FinalAction", "PoolRunSnapshot", "PoolScanResult",
     "TickerScanResult", "TimingStatus", "OptionsStatus",
     "run_pcs_pool",
     "PoolEventResult", "evaluate_pool_event",
+    "SpreadCandidate", "shortlist_spreads",
 ]
